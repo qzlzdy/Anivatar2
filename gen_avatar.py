f170e8c5e576d8ee0dace5bac11675d959ec6cf8
qzlzdy <qzlzdy@yangvillage.top>
2025-03-02 18:41:24 +0800

    StyleGAN: fix glog issue
    define GLOG_USE_GLOG_EXPORT macro
    
    Signed-off-by: qzlzdy <qzlzdy@yangvillage.top>

--- gen_avatar.py
+++ gen_avatar.py
@@ -12,16 +12,18 @@ def main():
     print("Loading network from \"%s\"..." % network_pkl)
     with dnnlib.util.open_url(network_pkl) as f:
         G = legacy.load_network_pkl(f)["G_ema"]
+    G = G.cuda()
     os.makedirs(outdir, exist_ok=True)
-    label = torch.zeros([1, G.c_dim])
-    total = 16
+    label = torch.zeros([1, G.c_dim]).cuda()
+    total = 64
     for i in range(total):
         seed = time.time_ns() % (1 << 32)
         print("Generating avatar for seed %d (%d/%d)" % (seed, i + 1, total))
         z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim))
+        z = z.cuda()
         img = G(z, label)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-        PIL.Image.fromarray(img[0].numpy(), "RGB").save(f"{outdir}/{i:04d}.png")
+        PIL.Image.fromarray(img[0].cpu().numpy(), "RGB").save(f"{outdir}/{i:04d}.png")
 
 if __name__ == "__main__":
     main()
